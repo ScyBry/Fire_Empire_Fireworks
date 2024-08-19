bf116de7c48d653b01f00f8b33405ed35504d38a
Oleg <99564441+ScyBry@users.noreply.github.com>
2024-08-19 16:11:52 +0300

    fixed burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,34 +15,45 @@ export const Header = () => {
     <header className={styles.header}>
       <div className="container">
         <div className={styles.header__inner}>
-          <div className={styles.header__branding}>
-            <NavLink to="/">
-              <img src={Logo} alt="Лого" className={styles.logo} />
-            </NavLink>
+          <NavLink to="/">
+            <img src={Logo} alt="Лого" className={styles.logo} />
+          </NavLink>
 
-            <IconButton className={styles.burgerMenu} onClick={toggleMenu}>
+          <div className={styles.burgerMenu}>
+            <IconButton onClick={toggleMenu}>
               {isMenuOpen ? (
                 <Close fontSize="large" />
               ) : (
                 <Menu fontSize="large" />
               )}
             </IconButton>
-
-            <nav
-              className={`${styles.navigation} ${isMenuOpen ? styles.navigation_open : ""}`}
-            >
-              {navLinks.map((link) => (
-                <NavLink
-                  key={link.name}
-                  to={link.link}
-                  className={`hover-underline-animation ${styles.navigation__link}`}
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  {link.name}
-                </NavLink>
-              ))}
-            </nav>
           </div>
+
+          <nav
+            className={`${styles.navigation} ${isMenuOpen ? styles.navigation_open : ""}`}
+          >
+            {navLinks.map((link) => (
+              <NavLink
+                key={link.name}
+                to={link.link}
+                className={`hover-underline-animation ${styles.navigation__link}`}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {link.name}
+              </NavLink>
+            ))}
+            <div className={styles.contact__info}>
+              <div className={styles.contact__item}>
+                <LocationOn fontSize="small" />
+                <p>Гродненская область, Берестовицкий р-н, д. Лесная</p>
+              </div>
+              <div className={styles.contact__item}>
+                <Phone fontSize="small" />
+                <p>8 (01511) 7-03-37</p>
+              </div>
+            </div>
+          </nav>
+
           <div className={styles.contact__info}>
             <div className={styles.contact__item}>
               <LocationOn fontSize="small" />
