4b9928eebbe0c335bb6a97e477c6dc6f334279c8
Oleg <99564441+ScyBry@users.noreply.github.com>
2024-08-20 13:47:12 +0300

    update

---
+++ src/components/CoalProducts/CoalProductCard/CoalProductCard.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import { motion } from "framer-motion";
+import styles from "./styles.module.sass";
+import { Button } from "../../Button/Button.tsx";
+
+interface CoalProductCardProps {
+  imgSrc: string;
+  imgAlt: string;
+  title: string;
+  description: string;
+  reverse?: boolean;
+  handleModal: () => void;
+}
+
+export const CoalProductCard: FC<CoalProductCardProps> = ({
+  imgSrc,
+  imgAlt,
+  title,
+  description,
+  reverse = false,
+  handleModal,
+}) => {
+  return (
+    <motion.article
+      initial={{ opacity: 0, x: reverse ? 100 : -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+      className={`${styles.coalProduct} ${reverse ? styles.reverseOrder : ""}`}
+    >
+      <div className={styles.coalProduct__image}>
+        <img src={imgSrc} alt={imgAlt} />
+      </div>
+      <div className={styles.coalProduct__details}>
+        <h3 className={styles.coalProduct__title}>{title}</h3>
+        <div className={styles.coalProduct__description}>
+          {description.split("\n").map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+        <Button onClick={handleModal}>Заказать</Button>
+      </div>
+    </motion.article>
+  );
+};
